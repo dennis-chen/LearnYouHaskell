02b2b1a6939f083566194f85dfd0c8b9495f6b11
Dennis Chen <dennis.chen@students.olin.edu>
2015-12-17 13:01:10 -0500

    added row and column checking

--- LearnYouHaskell/Chapters/ticTacToe.hs
+++ LearnYouHaskell/Chapters/ticTacToe.hs
@@ -20,6 +20,14 @@ interleave xs [] = xs
 interleave [] ys = ys
 interleave (x:xs) (y:ys) = x : y : interleave xs ys
 
+--getDia :: Int -> Board -> [Maybe (Maybe Mark)]
+--getDia n b = map ((flip M.lookup) b) coords
+--    where coords = [(x,y) | x<-[0..2], y<-[n]]
+
+getCol :: Int -> Board -> [Maybe (Maybe Mark)]
+getCol n b = map ((flip M.lookup) b) coords
+    where coords = [(x,y) | x<-[0..2], y<-[n]]
+
 getRow :: Int -> Board -> [Maybe (Maybe Mark)]
 getRow n b = map ((flip M.lookup) b) coords
     where coords = [(x,y) | x<-[n], y<-[0..2]]
@@ -66,16 +74,45 @@ temp b = filter (\(k,v) -> v == Nothing) $ M.toList b
 placeMark :: Mark->Board->(Int,Int)->Board
 placeMark m b c = M.insert c (Just m) b
 
-playGame :: Board -> IO()
-playGame b = do 
-    printBoard b
+getAndPlaceMark :: Board -> IO Board
+getAndPlaceMark b = do
     print "Enter coordinate where you would like to place your X as x,y"
     coords <- getCommand
     if isValid coords b then
-        playGame (placeMark X b coords)
+        return (placeMark X b coords)
     else do
-        print "Already taken!"
-        playGame b
+        print "Invalid move!"
+        getAndPlaceMark b
+
+vectorOverMark :: [Maybe (Maybe Mark)] -> Mark -> Bool
+vectorOverMark xs m = foldr (\x accum-> if x == Just (Just m) then accum else False) True xs
+
+vectorOver :: [Maybe (Maybe Mark)] -> Bool
+vectorOver v = vectorOverMark v X || vectorOverMark v O
+
+rowOver:: Board -> Bool
+rowOver b = or $ map vectorOver $ map ((flip getRow) b) [0..2]
+
+colOver:: Board -> Bool
+colOver b = or $ map vectorOver $ map ((flip getCol) b) [0..2]
+
+--diaOver:: Board -> Bool
+--diaOver b = or $ map vectorOver $ map ((flip getDia) b) [0..2]
+
+gameOver :: Board -> Bool
+gameOver b = rowOver b || colOver b 
+
+--computerPlaceMark 
+
+playGame :: Board -> IO()
+playGame b = do 
+    printBoard b
+    newB <- getAndPlaceMark b
+    if gameOver newB then do
+        print "Game over!"
+        return ()
+    else
+        playGame newB
 
 main = do 
     playGame initialBoard
